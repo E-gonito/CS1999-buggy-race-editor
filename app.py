5d880f0f44e228e87b1fc63d1c751f8998ea1f01
E-gonito <masterdank47@gmail.com>
2020-06-08 18:24:50 +0100

    Added loading current record to form

--- app.py
+++ app.py
@@ -21,7 +21,12 @@ def home():
 @app.route('/new', methods = ['POST', 'GET'])
 def create_buggy():
   if request.method == 'GET':
-    return render_template("buggy-form.html")
+      con = sql.connect(DATABASE_FILE)
+      con.row_factory = sql.Row
+      cur = con.cursor()
+      cur.execute("SELECT * FROM buggies")
+      record = cur.fetchone();
+      return render_template("buggy-form.html", buggy=record)
   elif request.method == 'POST':
     all_colours = ('black', 'silver', 'gray', 'white', 'maroon', 'red', 'purple', 'fuchsia', 'green', 'lime', 'olive',
                    'yellow', 'navy', 'blue', 'teal', 'aqua', 'orange', 'aliceblue', 'antiquewhite', 'aquamarine',
